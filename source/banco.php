de4525bf104da5955d6751be452678f21569610c
CauaGraff <cauagraff@gmail.com>
2023-02-24 16:21:24 -0300

    banco

--- source/banco.php
+++ source/banco.php
@@ -1,5 +1,5 @@
 <?php
-
+// $mysqli = new mysqli("localhost", "u454850207_dati", "Banco_2020!", "u454850207_datilografia");
 $mysqli = new mysqli("localhost", "serv", "master", "datilografiabse");
 if ($mysqli->connect_error) {
     die("Não foi possível se conectar ao banco de dados");
